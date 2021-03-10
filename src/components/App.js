60ed7a28397de080dbbc46db98890689c742e997
nickarshadi <1477@holbertonschool.com>
2021-03-10 17:43:54 +0000

    Add to test.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../css/App.css';
+import React, { Component } from 'react';
+import About from './About.js'
+
+class App extends Component {
+
+  render () {
+    return (
+      <div> <About /> </div>
+    );
+  }
+}
+
+export default App;
